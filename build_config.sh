36c428ab2c7aeaaaa418f02297f1050975c4f70d
Nikita Tyunyayev <nikita.tyunyayev@uclouvain.be>
2024-08-29 09:48:43 +0200

    adding script to build config

---
+++ build_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ../zmk
+source .venv/bin/activate
+
+## Build left part
+west build --build-dir build/cradio/left --pristine --board nice_nano_v2 -s $HOME/Workspace/zmk/app -- -DSHIELD=cradio_left -DZMK_CONFIG=/home/ntyunyayev/zmk-config/config
+
+## Build right part
+west build --build-dir build/cradio/right --pristine --board nice_nano_v2 -s $HOME/Workspace/zmk/app -- -DSHIELD=cradio_right -DZMK_CONFIG=/home/ntyunyayev/zmk-config/config
