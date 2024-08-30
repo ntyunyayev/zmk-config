7ab2da70464517959774e4853f60fac83872987d
Nikita Tyunyayev <nikita.tyunyayev@uclouvain.be>
2024-08-30 13:39:19 +0200

    fixing hrm on other layers

--- build_config.sh
+++ build_config.sh
@@ -4,7 +4,7 @@ cd ../zmk
 source .venv/bin/activate
 
 ## Build left part
-west build --build-dir build/cradio/left --pristine --board nice_nano_v2 -s $HOME/Workspace/zmk/app -- -DSHIELD=cradio_left -DZMK_CONFIG=/home/ntyunyayev/zmk-config/config
+west build --build-dir build/cradio/left --pristine --board nice_nano_v2 -s $HOME/Workspace/zmk/app -- -DSHIELD=cradio_left -DZMK_CONFIG=$HOME/Workspace/zmk-config/config
 
 ## Build right part
-west build --build-dir build/cradio/right --pristine --board nice_nano_v2 -s $HOME/Workspace/zmk/app -- -DSHIELD=cradio_right -DZMK_CONFIG=/home/ntyunyayev/zmk-config/config
+west build --build-dir build/cradio/right --pristine --board nice_nano_v2 -s $HOME/Workspace/zmk/app -- -DSHIELD=cradio_right -DZMK_CONFIG=$HOME/Workspace/zmk-config/config
