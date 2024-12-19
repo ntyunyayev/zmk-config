f9e5e9c18766decb9a92a526e75b7a9ea87e3509
Nikita Tyunyayev <nikita.tyunyayev@uclouvain.be>
2024-12-19 09:48:15 +0100

    improved compilation script

--- build_config.sh
+++ build_config.sh
@@ -8,3 +8,8 @@ west build --build-dir build/cradio/left --pristine --board nice_nano_v2 -s $HOM
 
 ## Build right part
 west build --build-dir build/cradio/right --pristine --board nice_nano_v2 -s $HOME/Workspace/zmk/app -- -DSHIELD=cradio_right -DZMK_CONFIG=$HOME/Workspace/zmk-config/config
+
+mkdir -p /tmp/build
+
+mv build/cradio/left/zephyr/zmk.uf2 /tmp/build/cradio_left.uf2
+mv build/cradio/right/zephyr/zmk.uf2 /tmp/build/cradio_right.uf2
